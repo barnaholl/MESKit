e18db6a9d480f94733ef817d1a8db693d2cabc91
barnaholl <hollbarna@freemail.hu>
2021-01-07 17:45:42 +0100

    Init project

---
+++ src/main/java/com/theyellowpug/meskit/MeskitApplication.java
@@ -0,0 +1,13 @@
+package com.theyellowpug.meskit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeskitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeskitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/theyellowpug/meskit/MeskitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theyellowpug.meskit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeskitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
